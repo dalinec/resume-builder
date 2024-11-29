510e3bf35c940996f289f3fe60aa90f5fbd54304
dalinec <dalinec123@gmail.com>
2024-11-29 15:12:08 +0100

    add education model, schema, form - simmilar as prev work form

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -3,6 +3,7 @@ import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
+import EducationForm from "./forms/EducationForm";
 
 export const steps: {
   title: string;
@@ -23,5 +24,10 @@ export const steps: {
     title: "Work experience",
     component: WorkExperienceForm,
     key: "work-experience",
+  },
+  {
+    title: "Education",
+    component: EducationForm,
+    key: "education",
   },
 ];
