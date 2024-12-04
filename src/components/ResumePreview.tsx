4e1d1e005557049fb3d0aa951888e996de74f99c
dalinec <dalinec123@gmail.com>
2024-12-04 11:32:50 +0100

    added a custom useDimension hook that return the dims of the resume preview, added simple logic so the preview is resized accordingly, using zoom as a trick

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,7 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import { useRef } from "react";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -10,16 +12,28 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef);
+
   return (
     <div
+      ref={containerRef}
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        This text should change with the size of the container div
-      </h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{
+          zoom: (1 / 794) * width,
+        }}
+      >
+        <h1 className="p-6 text-3xl font-bold">
+          This text should change with the size of the container div
+        </h1>
+      </div>
     </div>
   );
 }
